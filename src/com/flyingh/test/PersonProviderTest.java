51153e16844e34159118f17e4d7a31428e62e9e3
flycoding <flycoding@yeah.net>
2013-01-19 17:59:01 +0800

    test PersonProvider's delete method.

--- src/com/flyingh/test/PersonProviderTest.java
+++ src/com/flyingh/test/PersonProviderTest.java
@@ -5,13 +5,25 @@ import android.net.Uri;
 import android.test.AndroidTestCase;
 
 public class PersonProviderTest extends AndroidTestCase {
+	private String uri;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		uri = "content://com.flyingh.providers.PersonProvider/person";
+	}
+
 	public void testInsert() {
-		Uri url = Uri
-				.parse("content://com.flyingh.providers.PersonProvider/person");
+		Uri url = Uri.parse(uri);
 		ContentValues values = new ContentValues();
 		values.put("name", "xiix");
 		values.put("age", 17);
 		values.put("amount", 5000);
 		getContext().getContentResolver().insert(url, values);
+	}
+
+	public void testDelete() {
+		getContext().getContentResolver().delete(Uri.parse(uri), "id=?",
+				new String[] { String.valueOf(3) });
 	}
 }
